32f5f4a2aef64be1491d3778e26bae96dd4f684b
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-11-12 00:21:07 +0800

    next seo for site

--- src/screens/airdrop/index.tsx
+++ src/screens/airdrop/index.tsx
@@ -15,6 +15,7 @@ import AirdropDesktopSlogan from "@assets/airdrop-slogan-desktop.svg";
 import TickIcon from "@assets/tick.svg";
 import AirdropParachuteDSM from "@assets/airdrop-dsm.svg";
 import { useGetScreenSizeHook } from "@hooks";
+import { NextSeo } from "next-seo";
 
 const Airdrop = () => {
   const { t, lang } = useTranslation("common");
@@ -48,150 +49,177 @@ const Airdrop = () => {
   }, [address]);
 
   return (
-    <Layout className={`${classes.root}`}>
-      <Box
-        display="flex"
-        justifyContent="center"
-        padding={isDesktop ? "40px 24px 0" : "72px 32px 24px"}
-      >
-        <Box className="dpm-page__container">
-          <Box
-            width={isDesktop ? "50%" : "100%"}
-            className="dpm-page__left-container"
-          >
-            {isDesktop ? (
-              <AirdropDesktopSlogan />
-            ) : (
-              <>
-                <AirdropSlogan />
-                <Box display="flex" justifyContent="center">
-                  <AirdropParachuteDSM width="300px" height="355px" />
-                </Box>
-              </>
-            )}
-
-            <Typography style={{ lineHeight: "24px" }}>
-              Insert below your address and verify how many <strong>DSM</strong>{" "}
-              have been allocated to you inside the upcoming{" "}
-              <strong>Desmos airdrop</strong>
-            </Typography>
-            <form
-              noValidate
-              onSubmit={(e) => {
-                e.preventDefault();
-                verify();
-              }}
+    <>
+      <NextSeo
+        title="DSM Airdrop"
+        description="Insert below your address and verify how many DSM have been allocated to you inside the upcoming Desmos airdrop"
+        canonical="https://airdrop.desmos.network/"
+        openGraph={{
+          url: "https://airdrop.desmos.network/",
+          title: "DSM Airdopr",
+          description:
+            "Insert below your address and verify how many DSM have been allocated to you inside the upcoming Desmos airdrop",
+          images: [
+            {
+              url: `${process.env.NEXT_PUBLIC_URL}/images/desmos.png`,
+            },
+          ],
+          site_name: "DSM Airdrop",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
+      <Layout className={`${classes.root}`}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          padding={isDesktop ? "40px 24px 0" : "72px 32px 24px"}
+        >
+          <Box className="dpm-page__container">
+            <Box
+              width={isDesktop ? "50%" : "100%"}
+              className="dpm-page__left-container"
             >
-              <Typography style={{ padding: "0 16px" }}>
-                Please insert your address
-              </Typography>{" "}
-              <Box display="flex" flexDirection={isDesktop ? "row" : "column"}>
-                <TextField
-                  error={error}
-                  helperText={error ? "Invalid address" : undefined}
-                  className={classes.searchBarStyle}
-                  style={{
-                    padding: isDesktop ? "0 0 16px 16px" : "0 16px 16px 16px",
-                    width: isDesktop ? "70%" : "auto",
-                  }}
-                  fullWidth={isDesktop ? undefined : true}
-                  variant="outlined"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  placeholder="Cosmos/Kava/Regen... address"
-                />
-                <Box
-                  padding={isDesktop ? 0 : "16px"}
-                  width={isDesktop ? "20%" : "auto"}
-                >
-                  <Button
-                    type="submit"
-                    fullWidth={isDesktop ? undefined : true}
-                    variant="contained"
-                    color="primary"
-                    style={{
-                      backgroundColor: "rgba(237, 108, 83, 1)",
-                      height: isDesktop ? "auto" : "2.5rem",
-                      padding: isDesktop ? "16px 14px" : 0,
-                      borderRadius: isDesktop ? "0px 4px 4px 0px" : "4px",
-                      marginLeft: isDesktop ? "-1px" : 0,
-                    }}
-                  >
-                    {loading ? (
-                      <CircularProgress color="inherit" size="24px" />
-                    ) : (
-                      "Calculate"
-                    )}
-                  </Button>
-                </Box>
-              </Box>
-            </form>
-            {verifyData !== null && (
-              <Box>
-                <Typography>The allocated DSM amount is</Typography>
-                <Typography style={{ paddingLeft: "16px" }} variant="h3">
-                  {verifyData.dsm_allotted} DSM
-                </Typography>
-              </Box>
-            )}
-            {dataStakingInfo !== null && dataStakingInfo !== undefined ? (
-              <Box>
-                {dataStakingInfo.map((item, key) => {
-                  const chain = item.chain_name;
-                  // console.log(dataStakingInfo);
-                  return (
-                    <Box
-                      display="flex"
-                      flexDirection="row"
-                      pl="16px"
-                      pt="16px"
-                      key={key}
-                    >
-                      <Box pr="8px">
-                        <TickIcon />
-                      </Box>
-                      <Typography
-                        style={{ color: "rgba(237, 108, 83, 1)", padding: 0 }}
-                      >
-                        {chain} Staker{" "}
-                        {item.forbole_delegator ? "& Forbole Delegator" : null}
-                      </Typography>
-                    </Box>
-                  );
-                })}
-              </Box>
-            ) : null}
-            {lpInfos !== null && lpInfos !== undefined ? (
-              <Box>
-                {lpInfos.map((item, key) => {
-                  const chain = item.chain_name;
-                  return (
-                    <Box
-                      display="flex"
-                      flexDirection="row"
-                      pl="16px"
-                      pt="16px"
-                      key={key}
-                    >
-                      <Box pr="8px">
-                        <TickIcon />
-                      </Box>
-                      <Typography
-                        style={{ color: "rgba(237, 108, 83, 1)", padding: 0 }}
-                      >
-                        {chain} LP Staker
-                      </Typography>
-                    </Box>
-                  );
-                })}
-              </Box>
-            ) : null}
-          </Box>
+              {isDesktop ? (
+                <AirdropDesktopSlogan />
+              ) : (
+                <>
+                  <AirdropSlogan />
+                  <Box display="flex" justifyContent="center">
+                    <AirdropParachuteDSM width="300px" height="355px" />
+                  </Box>
+                </>
+              )}
 
-          {isDesktop && <AirdropParachuteDSM width="650px" height="700px" />}
+              <Typography style={{ lineHeight: "24px" }}>
+                Insert below your address and verify how many{" "}
+                <strong>DSM</strong> have been allocated to you inside the
+                upcoming <strong>Desmos airdrop</strong>
+              </Typography>
+              <form
+                noValidate
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  verify();
+                }}
+              >
+                <Typography style={{ padding: "0 16px" }}>
+                  Please insert your address
+                </Typography>{" "}
+                <Box
+                  display="flex"
+                  flexDirection={isDesktop ? "row" : "column"}
+                >
+                  <TextField
+                    error={error}
+                    helperText={error ? "Invalid address" : undefined}
+                    className={classes.searchBarStyle}
+                    style={{
+                      padding: isDesktop ? "0 0 16px 16px" : "0 16px 16px 16px",
+                      width: isDesktop ? "70%" : "auto",
+                    }}
+                    fullWidth={isDesktop ? undefined : true}
+                    variant="outlined"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    placeholder="Cosmos/Kava/Regen... address"
+                  />
+                  <Box
+                    padding={isDesktop ? 0 : "16px"}
+                    width={isDesktop ? "20%" : "auto"}
+                  >
+                    <Button
+                      type="submit"
+                      fullWidth={isDesktop ? undefined : true}
+                      variant="contained"
+                      color="primary"
+                      style={{
+                        backgroundColor: "rgba(237, 108, 83, 1)",
+                        height: isDesktop ? "auto" : "2.5rem",
+                        padding: isDesktop ? "16px 14px" : 0,
+                        borderRadius: isDesktop ? "0px 4px 4px 0px" : "4px",
+                        marginLeft: isDesktop ? "-1px" : 0,
+                      }}
+                    >
+                      {loading ? (
+                        <CircularProgress color="inherit" size="24px" />
+                      ) : (
+                        "Calculate"
+                      )}
+                    </Button>
+                  </Box>
+                </Box>
+              </form>
+              {verifyData !== null && (
+                <Box>
+                  <Typography>The allocated DSM amount is</Typography>
+                  <Typography style={{ paddingLeft: "16px" }} variant="h3">
+                    {verifyData.dsm_allotted} DSM
+                  </Typography>
+                </Box>
+              )}
+              {dataStakingInfo !== null && dataStakingInfo !== undefined ? (
+                <Box>
+                  {dataStakingInfo.map((item, key) => {
+                    const chain = item.chain_name;
+                    // console.log(dataStakingInfo);
+                    return (
+                      <Box
+                        display="flex"
+                        flexDirection="row"
+                        pl="16px"
+                        pt="16px"
+                        key={key}
+                      >
+                        <Box pr="8px">
+                          <TickIcon />
+                        </Box>
+                        <Typography
+                          style={{ color: "rgba(237, 108, 83, 1)", padding: 0 }}
+                        >
+                          {chain} Staker{" "}
+                          {item.forbole_delegator
+                            ? "& Forbole Delegator"
+                            : null}
+                        </Typography>
+                      </Box>
+                    );
+                  })}
+                </Box>
+              ) : null}
+              {lpInfos !== null && lpInfos !== undefined ? (
+                <Box>
+                  {lpInfos.map((item, key) => {
+                    const chain = item.chain_name;
+                    return (
+                      <Box
+                        display="flex"
+                        flexDirection="row"
+                        pl="16px"
+                        pt="16px"
+                        key={key}
+                      >
+                        <Box pr="8px">
+                          <TickIcon />
+                        </Box>
+                        <Typography
+                          style={{ color: "rgba(237, 108, 83, 1)", padding: 0 }}
+                        >
+                          {chain} LP Staker
+                        </Typography>
+                      </Box>
+                    );
+                  })}
+                </Box>
+              ) : null}
+            </Box>
+
+            {isDesktop && <AirdropParachuteDSM width="650px" height="700px" />}
+          </Box>
         </Box>
-      </Box>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
