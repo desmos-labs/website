58dc458b884d640cc97564c100b9e5edc19b8dad
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-04-11 11:42:21 +0200

    build: added standalone output for nextjs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { i18n } = require("./next-i18next.config")
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  output: 'standalone',
   reactStrictMode: true,
   experimental: {
     // https://mmazzarolo.com/blog/2021-04-10-nextjs-scroll-restoration/
