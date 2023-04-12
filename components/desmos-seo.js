771092346af5ce06b4f702c11e37c69d35cd8890
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-04-12 11:30:32 +0200

    fix: added missing og:type tag

--- components/desmos-seo.js
+++ components/desmos-seo.js
@@ -7,6 +7,7 @@ export default function DesmosSeo({ url, title, description }) {
       description={description}
       canonical={url}
       openGraph={{
+        type: "website",
         url: url,
         title: title,
         description: description,
