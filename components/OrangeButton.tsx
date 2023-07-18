0f17bcc0aa09f575d22169f6e18bd6a5501dea8a
Alessandro Mazzon <alessandro@forbole.com>
2023-07-18 15:21:45 +0200

    feat: completed 404

--- components/OrangeButton.tsx
+++ components/OrangeButton.tsx
@@ -1,36 +1,55 @@
-import React from "react"
+import React, { useMemo } from "react"
+import Link from "next/link"
 
 export interface OrangeButtonProps {
   readonly buttonText: string
   readonly buttonLink: string
   readonly buttonMargin?: string
   readonly buttonPadding?: string
+  readonly isInternal?: boolean
 }
 
 const OrangeButton = (props: OrangeButtonProps) => {
-  const { buttonText, buttonLink, buttonMargin, buttonPadding } = props
-  return (
+  const { buttonText, buttonLink, buttonMargin, buttonPadding, isInternal } =
+    props
+
+  const content = useMemo(() => {
+    return (
+      <>
+        {buttonText}
+        <svg
+          className="group-hover:-rotate-45 transition-all ease-out duration-500 inline ml-1 align-middle"
+          width="12"
+          height="11"
+          viewBox="0 0 12 11"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6.20796 0.373974C6.4618 0.120134 6.87336 0.120134 7.1272 0.373974L11.7939 5.04064C12.0477 5.29448 12.0477 5.70604 11.7939 5.95988L7.1272 10.6265C6.87336 10.8804 6.4618 10.8804 6.20796 10.6265C5.95412 10.3727 5.95412 9.96115 6.20796 9.70731L9.76501 6.15026H0.667578C0.308593 6.15026 0.0175781 5.85925 0.0175781 5.50026C0.0175781 5.14128 0.308593 4.85026 0.667578 4.85026H9.76501L6.20796 1.29321C5.95412 1.03937 5.95412 0.627815 6.20796 0.373974Z"
+            fill="#EFEFEF"
+          />
+        </svg>
+        <div className="group-hover:opacity-0 bg-gradient-to-49 from-[#F07155] to-[#FFA756] absolute top-0 right-0 rounded w-full h-full -z-10 transition-all ease-out duration-500"></div>
+        <div className="bg-gradient-to-b from-[#FF9900] via-[#FF8A00] to-[#E86A50] absolute top-0 right-0 rounded w-full h-full -z-20 opacity-1"></div>
+      </>
+    )
+  }, [buttonText])
+
+  return isInternal ? (
+    <Link
+      href={buttonLink}
+      className={`${buttonMargin} ${buttonPadding} group relative text-[16px] leading-[24px] tracking-[.0015em] font-semibold box-content md:inline-block text-white z-10`}
+    >
+      {content}
+    </Link>
+  ) : (
     <a
       href={`${buttonLink}`}
       target="_blank"
       className={`${buttonMargin} ${buttonPadding} group relative text-[16px] leading-[24px] tracking-[.0015em] font-semibold box-content md:inline-block text-white z-10`}
     >
-      {buttonText}
-      <svg
-        className="group-hover:-rotate-45 transition-all ease-out duration-500 inline ml-1 align-middle"
-        width="12"
-        height="11"
-        viewBox="0 0 12 11"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M6.20796 0.373974C6.4618 0.120134 6.87336 0.120134 7.1272 0.373974L11.7939 5.04064C12.0477 5.29448 12.0477 5.70604 11.7939 5.95988L7.1272 10.6265C6.87336 10.8804 6.4618 10.8804 6.20796 10.6265C5.95412 10.3727 5.95412 9.96115 6.20796 9.70731L9.76501 6.15026H0.667578C0.308593 6.15026 0.0175781 5.85925 0.0175781 5.50026C0.0175781 5.14128 0.308593 4.85026 0.667578 4.85026H9.76501L6.20796 1.29321C5.95412 1.03937 5.95412 0.627815 6.20796 0.373974Z"
-          fill="#EFEFEF"
-        />
-      </svg>
-      <div className="group-hover:opacity-0 bg-gradient-to-49 from-[#F07155] to-[#FFA756] absolute top-0 right-0 rounded w-full h-full -z-10 transition-all ease-out duration-500"></div>
-      <div className="bg-gradient-to-b from-[#FF9900] via-[#FF8A00] to-[#E86A50] absolute top-0 right-0 rounded w-full h-full -z-20 opacity-1"></div>
+      {content}
     </a>
   )
 }
--- pages/404.tsx
+++ pages/404.tsx
@@ -4,6 +4,9 @@ import { useTranslation } from "next-i18next"
 import MainLayout from "@/layouts/MainLayout"
 import SectionLayout from "@/layouts/SectionLayout"
 import CenteredContentLayout from "@/layouts/CenteredContentLayout"
+import Image from "next/image"
+import mainImage from "@/public/404.png"
+import OrangeButton from "@/components/OrangeButton"
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
   props: {
@@ -25,16 +28,27 @@ const Custom404 = () => {
         fullScreenHeightOption="always"
         sectionBackground="bg-gradient-orange-mobile md:bg-gradient-orange-tablet lg:bg-gradient-orange-desktop bg-cover bg-center"
       >
-        <CenteredContentLayout>
-          <div className="text-desmos-white-Default text-center">
-            <h1 className="xl:text-[88px] lg:text-[80px] md:text-[56px] text-[44px] lg:leading-[116px] md:leading-[84px] leading-[66px] -tracking-[.015em] font-bold">
-              {t("title404")}
-            </h1>
-            <p className="lg:text-[18px] md:text-[16px] text-[14px] lg:leading-[27px] md:leading-[32px] leading-[24px] tracking-[.0015em] pt-6 pb-10">
-              {t("description404")}
-            </p>
+        <div className="flex flex-col text-desmos-white-Default text-center items-center pt-[160px] md:pt-[60px]">
+          <Image
+            src={mainImage}
+            alt={"404"}
+            className="w-[248px] md:w-[408px] xl:w-[690px] pb-4"
+          />
+          <h1 className="xl:text-[88px] lg:text-[80px] md:text-[56px] text-[28px] font-[600]">
+            {t("oops")}
+          </h1>
+          <p className="lg:text-[20px] md:text-[16px] text-[14px] lg:leading-[27px] md:leading-[32px] leading-[24px] tracking-[.0015em] pb-10 md:py-12">
+            {t("description404")}
+          </p>
+          <div>
+            <OrangeButton
+              buttonText={t("goToHomepage")}
+              buttonLink={"/"}
+              buttonPadding="px-16 py-2.5"
+              isInternal={false}
+            />
           </div>
-        </CenteredContentLayout>
+        </div>
       </SectionLayout>
     </MainLayout>
   )
