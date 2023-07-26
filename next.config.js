0ad02a0ec6be89fb4d75f3b944a861ff7a35d5da
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-07-26 11:00:23 -0500

    build: add assetPrefix to solve DPM images not being loaded properly

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const { i18n } = require("./next-i18next.config")
 
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
+/** @type {import("next").NextConfig} */
 
 module.exports = {
   output: "standalone",
   reactStrictMode: true,
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? "https://desmos.network" : undefined,
   experimental: {
     // https://mmazzarolo.com/blog/2021-04-10-nextjs-scroll-restoration/
     scrollRestoration: true,
